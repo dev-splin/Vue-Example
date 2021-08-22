162db44fc6e84f2329d075919d4f9de81c749fbb
Splin <dev.splin@gmail.com>
2021-08-22 19:04:13 +0900

    ADD : [3장] quiz - 03-2, 03-3 추가

--- quiz/03-2/sample/js/app.js
+++ quiz/03-2/sample/js/app.js
@@ -1,5 +1,8 @@
 // 실습 #1 - `todo-footer` 컴포넌트 전역 등록
 // <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+Vue.component('todo-footer', {
+  template : '<p>This is another child global component</p> '
+})
 
 var app = new Vue({
   el: '#app',
@@ -9,4 +12,9 @@ var app = new Vue({
 
   // 실습 #2 - `todo-list` 컴포넌트 지역 등록
   // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+  components : {
+    'todo-list' : {
+      template : '<p>This is another child local component</p>'
+    }
+  }
 });
